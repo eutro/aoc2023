4e6c18723dd07874079280b7f41b5fc8eb4ee980
eutro <git@eutro.dev>
2023-12-10 02:12:09 +0000

    Wow! Proofs!

--- src/Day09.v
+++ src/Day09.v
@@ -1,8 +1,7 @@
 From Coq Require Export String List ZArith.
-From Coq Require Import Lia Ascii Sorting Orders OrdersEx OrdersAlt Init.Byte.
+From Coq Require Import Lia Program.Wf.
 
 Export ListNotations.
-Import IfNotations.
 
 #[global] Open Scope Z_scope.
 
@@ -19,15 +18,51 @@ Notation "n" := n (in custom integer at level 0, n constr at level 0).
 
 Definition list_diff (xs ys : list Z) : list Z :=
   List.map (fun '(x, y) => x - y) (List.combine xs ys).
+
 Definition list_der (xs : list Z) : list Z := list_diff xs (tl xs).
 
-Fixpoint step_seq_ (len : nat) (xs : list Z) : Z :=
+Lemma cons_length {A : Type} : forall (x : A) (xs : list A), List.length (x :: xs) = S (List.length xs).
+Proof. intros x xs. unfold length. reflexivity. Qed.
+Lemma tl_length {A : Type} : forall (xs : list A), List.length (tl xs) = pred (List.length xs).
+Proof.
+  intros xs.
+  unfold tl.
+  destruct xs as [ | _h xtl ].
+  - reflexivity.
+  - rewrite cons_length, Nat.pred_succ.
+    reflexivity.
+Qed.
+
+Lemma list_der_length: forall xs, List.length (list_der xs) = pred (List.length xs).
+Proof.
+  intros xs.
+  unfold list_der, list_diff.
+  rewrite map_length, combine_length, tl_length.
+  set (len := List.length xs).
+  refine (min_r len (pred len) _).
+  unfold pred.
+  destruct len.
+  - reflexivity.
+  - apply Peano.le_S.
+    reflexivity.
+Qed.
+
+Program Fixpoint step_seq_ (xs : list Z) {measure (List.length xs)} : Z :=
   if List.forallb (Z.eqb 0) xs then 0 else
-    match len with
-    | O => 0
-    | S len' => List.hd 0 xs + (step_seq_ len' (list_der xs))
+    match xs with
+    | [] => 0
+    | h::_ => h + (step_seq_ (list_der xs))
     end.
-Definition step_seq_back (xs : list Z) : Z := step_seq_ (List.length xs) xs.
+Obligation 1.
+Proof.
+  rewrite list_der_length.
+  unfold length.
+  rewrite Nat.pred_succ.
+  unfold Peano.lt.
+  reflexivity.
+Qed.
+
+Definition step_seq_back (xs : list Z) : Z := step_seq_ xs.
 Definition step_seq (xs : list Z) : Z := step_seq_back (List.rev xs).
 
 Definition part (f : list Z -> Z) (inp : list (list Z)) :=
