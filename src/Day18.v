ae279642fee93ba684ea71f2b50d343214c3cd79
eutro <git@eutro.dev>
2023-12-19 16:05:12 +0000

    Day 18

--- src/Day18.v
+++ src/Day18.v
@@ -24,8 +24,7 @@ Module Backport_PosnOrder := Backport_OT PosnOrder.
 Module PosnMap := FMapAVL.Make Backport_PosnOrder.
 Definition posn : Type := PosnMap.E.t.
 
-Definition dig_map : Type := PosnMap.t int.
-Definition dig_state : Type := posn * dig_map.
+Definition dig_map : Type := list posn.
 
 Inductive dir : Set := right | left | down | up.
 
@@ -57,73 +56,32 @@ Definition offset_by (p : posn) (d : dir) (sf : int) : posn :=
   end.
 Definition offset p d := offset_by p d 1.
 
-Fixpoint dig2 (m : dig_map) (p : posn) (i : insn) (rem : nat) : dig_state :=
-  match rem with
-  | O => (p, m)
-  | S rem' =>
-      let p' := offset p (direction i) in
-      let m' := PosnMap.add p' (colour i) m in
-      dig2 m' p' i rem'
+Definition dig (insns : list insn) : dig_map :=
+  let '(x, m) :=
+    fold_left
+      (fun '(p, m) insn =>
+         let p' := offset_by p (direction insn) (distance insn) in
+         (p', p :: m))
+      insns
+      ((0, 0), []) in
+  x :: m.
+
+Fixpoint area_ (acc : int) (m : dig_map) : int :=
+  match m with
+  | p1 :: ((p2 :: _) as m') =>
+      let '((x1, y1), (x2, y2)) := (p1, p2) in
+      let det := y1 * x2 - x1 * y2 in
+      let edge := abs (x2 - x1) + abs (y2 - y1) in
+      area_ (acc + det + edge) m'
+  | _ => acc
   end.
-Definition dig1 (m : dig_state) (i : insn) : dig_state :=
-  let '(pos, map) := m in
-  dig2 map pos i (Z.to_nat (to_Z (distance i))).
-Fixpoint dig (m : dig_state) (insns : list insn) : dig_state :=
-  match insns with
-  | [] => m
-  | insn :: insns' => dig (dig1 m insn) insns'
-  end.
+Definition area (m : dig_map) : int :=
+  (area_ 0 m) / 2 + 1.
 
-Definition start_dig (insns : list insn) : dig_map :=
-  let m := ((0, 0), PosnMap.empty int) in
-  snd (dig m insns).
+Definition part1 (m : list insn) := area (dig m).
+Definition part2 (m : list insn) := area (dig (map flip_insn m)).
 
-Definition min (a b : int) : int := if a <? b then a else b.
-Definition max (a b : int) : int := if a <? b then b else a.
-
-Definition get_size (pmin pmax : posn) : int :=
-  let '((minx, miny), (maxx, maxy)) := (pmin, pmax) in
-  (maxx + 1 - minx) * (maxy + 1 - miny).
-
-Definition in_bounds (pmin pmax p : posn) : bool :=
-  let '((minx, miny), (maxx, maxy)) := (pmin, pmax) in
-  let '(x, y) := p in
-  (minx <=? x) && (x <=? maxx) &&
-    (miny <=? y) && (y <=? maxy).
-Fixpoint flood_ (pmin pmax : posn) (m : dig_map) (q : list posn) (fuel : nat) : dig_map :=
-  match (fuel, q) with
-  | (O, _) | (_, []) => m
-  | (S fuel', p :: q') =>
-      let is_ok p := in_bounds pmin pmax p && negb (PosnMap.mem p m) in
-      let neighbours := filter is_ok (map (offset p) [left; right; up; down]) in
-      let q' := neighbours ++ q' in
-      let m' := fold_left (fun m p => PosnMap.add p (-1) m) neighbours m in
-      flood_ pmin pmax m' q' fuel'
-  end.
-Definition flood (m : dig_map) (start : posn) (pmin pmax : posn) : dig_map :=
-  let sz_n := Z.to_nat (to_Z (get_size pmin pmax)) in
-  flood_ pmin pmax (PosnMap.add start (-1) m) [start] sz_n.
-
-Definition fill_count (m : dig_map) :=
-  let extreme f :=
-    PosnMap.fold
-      (fun k v best =>
-         (f (fst best) (fst k),
-           f (snd best) (snd k)))
-      m (0, 0) in
-  let '(pmin, pmax) := (extreme min, extreme max) in
-  let pmin' := offset (offset pmin left) up in
-  let pmax' := offset (offset pmax right) down in
-  let flooded := flood m pmin' pmin' pmax' in
-  get_size pmin' pmax' -
-    of_Z (Z.of_nat (PosnMap.cardinal flooded)) +
-    of_Z (Z.of_nat (PosnMap.cardinal m)).
-
-Definition part1 (m : list insn) :=
-  let dug := start_dig m in
-  fill_count dug.
-
-Definition main (m : list insn) := part1 m.
+Definition main (m : list insn) := (part1 m, part2 m).
 
 Definition int_of_ascii (a : ascii) : int :=
   of_Z (Z.of_N (N_of_ascii a)).
@@ -173,4 +131,5 @@ L 2 (#"015232")
 U 2 (#"7a21e3")
 .
 
-Compute main example.
+Example sample: main example = (62, 952408144115).
+Proof. reflexivity. Qed.
